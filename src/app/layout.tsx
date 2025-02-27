d90555c70fec506761cac51ee91e7deec22cfb9e
Ankit Kaushik <ankitkaushik6269@gmail.com>
2025-02-27 10:32:52 +0530

    done some seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,6 @@ export const metadata: Metadata = {
   keywords: [
     "Ankit Kaushik",
     "Ankit Kaushik portfolio",
-    "Ankit",
-    "Kaushik",
     "Full Stack Developer",
     "Web Developer",
     "JavaScript",
@@ -22,18 +20,12 @@ export const metadata: Metadata = {
     "Software Engineer",
     "UI/UX Designer",
     "Tech Enthusiast",
-    "Ankit Kaushik Vercel",
-    "C++",
-    "MongoDB",
-    "Sanity",
-    "HTML",
-    "CSS"
   ],
   openGraph: {
     type: 'website',
     url: 'https://kaushikankit.vercel.app',
     title: 'Ankit Kaushik - Portfolio',
-    description: "Explore Ankit Kaushik's portfolio, highlighting expertise in Next.js, React, TypeScript, JavaScript, C++, MongoDB, Sanity, HTML, CSS, and other technologies. Discover a range of projects showcasing web development skills, coding proficiency, and innovative solutions. Dive into a comprehensive display of Ankit's technical capabilities and accomplishments in software development and web design.",
+    description: "Explore Ankit Kaushik's portfolio, highlighting expertise in Next.js, React, TypeScript, JavaScript, C++, MongoDB, Sanity, HTML, CSS, and other technologies.",
     images: [
       {
         url: 'https://kaushikankit.vercel.app/me.png',
@@ -47,26 +39,28 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     site: '@Ankit_kaushik0',
     title: 'Ankit Kaushik - Portfolio',
-    description: "Explore Ankit Kaushik's portfolio, highlighting expertise in Next.js, React, TypeScript, JavaScript, C++, MongoDB, Sanity, HTML, CSS, and other technologies. Discover a range of projects showcasing web development skills, coding proficiency, and innovative solutions. Dive into a comprehensive display of Ankit's technical capabilities and accomplishments in software development and web design.",
+    description: "Explore Ankit Kaushik's portfolio and projects in Next.js, React, TypeScript, and other technologies.",
     images: [
       {
         url: 'https://kaushikankit.vercel.app/me.png',
         alt: 'Ankit Kaushik Portfolio'
       }
     ]
-  }
+  },
+  verification: {
+    google: "IhleRkwS3CAPHzFgSf0ySPwCj7IYM1DdenB4JQzDb28" 
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <head>
+        <meta name="google-site-verification" content="IhleRkwS3CAPHzFgSf0ySPwCj7IYM1DdenB4JQzDb28" />
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
